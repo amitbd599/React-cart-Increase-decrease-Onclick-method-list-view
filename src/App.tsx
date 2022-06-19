3e429bbeec57befa9145518a4ef875c6bfc09bfd
amitbd599 <amitbd599@gmail.com>
2022-06-19 18:34:10 -0400

    add all file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { FiMinus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
+import { GrClose } from "react-icons/gr";
 
-function App() {
+const UpdateData = ({ value }: any) => {
+  const [hold, setHold] = useState(0);
+
+  const hendelIncrease = () => {
+    setHold(hold + 1);
+  };
+  const hendelDecrease = () => {
+    if (hold > 0) {
+      setHold(hold - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="UpdateData">
+      <span className="item">{value.title} </span>
+      <button className="item" onClick={hendelDecrease}>
+        -
+      </button>
+      <span className="item">{hold}</span>
+      <button className="item" onClick={hendelIncrease}>
+        +
+      </button>
     </div>
   );
-}
+};
+
+const productData = [
+  { id: 1, title: "Product One", price: 50 },
+  { id: 2, title: "Product Two", price: 150 },
+];
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="main">
+        <h2>React Increase Decrease value List View</h2>
+        <div>
+          {productData.map((value) => {
+            return <UpdateData value={value} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
